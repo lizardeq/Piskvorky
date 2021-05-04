45bdf7ac88237c811040c061301c7b3b1330c294
Jana Sebestova <lizarde@seznam.cz>
2021-05-04 13:01:15 +0200

    ukol 4.3

--- index.js
+++ index.js
@@ -7,16 +7,12 @@ const klik = (event) => {
   if (tah === 'kolecko') {
     hraje.src="img/cross.svg";
     hraje.alt="Bily krizek";
-    // hraje.classList.add("krizek");
-    // hraje.classList.remove("kolecko");
     event.target.classList.add('pole--kolecko');
     event.target.disabled = true;
     tah = 'krizek';
   } else {
     hraje.src="img/circle.svg";
     hraje.alt="Bile kolecko";
-    // hraje.classList.add("kolecko");
-    // hraje.classList.remove("krizek");
     event.target.classList.add('pole--krizek');
     event.target.disabled = true;
     tah = 'kolecko';
