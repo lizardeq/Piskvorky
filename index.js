af2a64008149ac3ac3f82e01d1d37293a88f6ae6
Jana Sebestova <lizarde@seznam.cz>
2021-05-10 00:11:54 +0200

    ukol pet.jedna

--- index.js
+++ index.js
@@ -19,10 +19,105 @@ const klik = (event) => {
   }
 }
 
-
-
 const pole = document.querySelectorAll('.btn');
 for (let i = 0; i < pole.length; i++) {
 pole[i].addEventListener('click', klik);
 };
 
+
+//vyhodnoceni vyhry
+
+const getSymbol = (field) => {
+	if (field.classList.contains('pole--krizek')) {
+		return 'krizek'
+	} else if (field.classList.contains('pole--kolecko')) {
+		return 'kolecko'
+	}
+}
+
+const boardSize = 10 // 10x10
+const fields = document.querySelectorAll('.btn') 
+const getField = (row, column) => fields[row * boardSize + column]
+
+const getPosition = (field) => {
+	let fieldIndex = 0
+	while (fieldIndex < fields.length && field !== fields[fieldIndex]) {
+		fieldIndex++
+	}
+
+	return {
+		row: Math.floor(fieldIndex / boardSize),
+		column: fieldIndex % boardSize,
+	}
+}
+
+const symbolsToWin = 5
+const isWinningMove = (field) => {
+	const origin = getPosition(field)
+	const symbol = getSymbol(field)
+  
+
+	let i
+
+	let inRow = 1 // Jednička pro právě vybrané políčko
+	// Doleva
+	i = origin.column
+	while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
+		inRow++
+		i--
+	}
+
+	// Doprava
+	i = origin.column
+	while (
+		i < boardSize - 1 &&
+		symbol === getSymbol(getField(origin.row, i + 1))
+	) {
+		inRow++
+		i++
+	}
+
+	if (inRow >= symbolsToWin) {
+		return true
+	}
+
+	let inColumn = 1
+	// Nahoru
+	i = origin.row
+	while (i > 0 && symbol === getSymbol(getField(i - 1, origin.column))) {
+		inColumn++
+		i--
+	}
+
+	// Dolu
+	i = origin.row
+	while (
+		i < boardSize - 1 &&
+		symbol === getSymbol(getField(i + 1, origin.column))
+	) {
+		inColumn++
+		i++
+	}
+
+	if (inColumn >= symbolsToWin) {
+		return true
+	}
+
+	return false
+}
+
+if (isWinningMove(5)) {
+  setTimeout(() => {
+    if (player === 'krizek') {
+      const conf = confirm(`Vyhrává kolečko.`);
+      if (conf) {
+        location.reload();
+      }
+    } else {
+      const conf = confirm(`Vyhrává křížek.`);
+      if (conf) {
+        location.reload();
+      }
+    }
+  }, 250);
+};
