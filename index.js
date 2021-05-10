d276a81f081c32165599602e0d42942b48f792de
Jana Sebestova <lizarde@seznam.cz>
2021-05-11 01:43:20 +0200

    5.3deadEnd

--- index.js
+++ index.js
@@ -3,26 +3,26 @@
 let tah = 'kolecko';
 const hraje = document.querySelector('#znacka'); 
 
-const klik = (event) => {
-  if (tah === 'kolecko') {
-    hraje.src="img/cross.svg";
-    hraje.alt="Bily krizek";
-    event.target.classList.add('pole--kolecko');
-    event.target.disabled = true;
-    tah = 'krizek';
-  } else {
-    hraje.src="img/circle.svg";
-    hraje.alt="Bile kolecko";
-    event.target.classList.add('pole--krizek');
-    event.target.disabled = true;
-    tah = 'kolecko';
-  }
-}
+// let klik = (event) => {
+//   if (tah === 'kolecko') {
+//     hraje.src="img/cross.svg";
+//     hraje.alt="Bily krizek";
+//     event.target.classList.add('pole--kolecko');
+//     event.target.disabled = true;
+//     tah = 'krizek';
+//   } else {
+//     hraje.src="img/circle.svg";
+//     hraje.alt="Bile kolecko";
+//     event.target.classList.add('pole--krizek');
+//     event.target.disabled = true;
+//     tah = 'kolecko';
+//   }
+// };
 
-const pole = document.querySelectorAll('.btn');
-for (let i = 0; i < pole.length; i++) {
-pole[i].addEventListener('click', klik);
-};
+// const pole = document.querySelectorAll('.btn');
+// for (let i = 0; i < pole.length; i++) {
+// pole[i].addEventListener('click', klik);
+// };
 
 
 //vyhodnoceni vyhry
@@ -107,17 +107,49 @@ const isWinningMove = (field) => {
 }
 
 
-pole.forEach(btnItem => btnItem.addEventListener('click', (event) => {klik(event);
-let winner = '';
-if (isWinningMove(event.target) === true) {
-  if (hraje === 'kolecko') {
-    winner = 'Křížek';
-  } else (hraje === 'krizek') {
-    winner = 'Kolečko';
+// const pole2 = document.querySelectorAll('.btn');
+// for (let i = 0; i < pole.length; i++) {
+// pole2[i].addEventListener('click', (event)=>{isWinningMove(event.target);
+// if (isWinningMove === true) {console.log(true)};
+// });
+// };
+
+let klik = (event) => {
+  if (tah === 'kolecko') {
+    hraje.src="img/cross.svg";
+    hraje.alt="Bily krizek";
+    event.target.classList.add('pole--kolecko');
+    event.target.disabled = true;
+    tah = 'krizek';
+		isWinningMove(event.target);
+		if (isWinningMove === true) {
+			console.log('Hura');
+		}
+  } else {
+    hraje.src="img/circle.svg";
+    hraje.alt="Bile kolecko";
+    event.target.classList.add('pole--krizek');
+    event.target.disabled = true;
+    tah = 'kolecko';
   }
-}
-let novaHra = confirm(`Vyhrává ${winner}. Chceš zkusit ještě jednou?`);
-if (novaHra === true) {
-  location.reload();
 };
-}));
+
+const pole = document.querySelectorAll('.btn');
+for (let i = 0; i < pole.length; i++) {
+pole[i].addEventListener('click', klik);
+};
+
+
+// let winner = '';
+// if (isWinningMove(event.target) === true) {
+//   if (hraje === 'kolecko') {
+//     winner = 'křížek';
+//   } else if (hraje === 'krizek') {
+//     winner = 'kolečko';
+//   }
+// }
+// let novaHra = confirm(`Vyhrává ${winner}. Chceš zkusit ještě jednou?`);
+// if (novaHra === true) {
+//   location.reload();
+// };
+// }));
